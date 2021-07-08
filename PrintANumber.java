6ed6101d609902f5774dfc50191d7497372e9cb3
NaureenJava <86628342+NaureenJava@users.noreply.github.com>
2021-07-08 21:11:15 +0530

    Add files via upload

---
+++ PrintANumber.java
@@ -0,0 +1,24 @@
+package curie;
+
+import java.util.Scanner;
+
+public class PrintANumber {
+	public static void main(String[]args) {
+		//creates a reader instance which takes
+		//input from standard input-keyboard
+		Scanner reader = new Scanner(System.in);
+		System.out.print ("Enter a number:");
+		// nextInt()reads the next integer from the keyboard
+	     int number = reader.nextInt();
+	     // println() prints the following line to the output screen
+	     System.out.println("you entered: " + number);
+	     
+	     
+	
+		
+		
+	
+	       
+	}
+
+}
---
+++ basic.java
@@ -0,0 +1,15 @@
+package curie;
+
+public class basic {
+	
+	public static void main(String [] args) {
+	//my input data
+	int i=100;
+	int j=200;
+	//processing
+	int s=i+j;
+	//output
+	System.out.println(s);
+	
+}
+}
