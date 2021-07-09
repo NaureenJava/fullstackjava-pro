3b88ae3d9bc50dd98d53ec3d6af663677a1f36e4
NaureenJava <86628342+NaureenJava@users.noreply.github.com>
2021-07-10 03:12:44 +0530

    Add files via upload

---
+++ QuotientRemainder.java
@@ -0,0 +1,19 @@
+package hit.day6;
+
+public class QuotientRemainder {
+	public static void main(String[]args) {
+		int dividend=25, divisor=4;
+		int quotient = dividend / divisor;
+		int remainder = dividend % divisor;
+		System.out.println("Quotient = +quotient");
+		System.out.println("Remainder = " +remainder);
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
---
+++ SwapTwoNumbersExercise.java
@@ -0,0 +1,25 @@
+package hit.day6;
+
+public class SwapTwoNumbersExercise {
+	public static void main(String[]args) {
+		float first = 2.50f ,second = 4.50f ;;
+
+		System.out.println("---Before swap--");
+		System.out.println("First number ="+ first);
+		System.out.println("Second number="+ second);
+		// value iof first is assigned to temporary
+		float temporary = first;
+		// value of second is assigned to gfirst
+		first =second;
+		// value of temporary (which contains the initial value of first)is assigned to second
+		second = temporary;
+		System.out.println(" --After swap --");
+		System.out.println("First number =" + first);
+		System.out.println("second number =" + second);
+		
+	}
+	
+}
+	
+
+
